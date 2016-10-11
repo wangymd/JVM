c33424b26477b21893e7ce262a490d4597a25f83
wangymd <wangymd@yonyou.com>
2016-10-11 16:55:38 +0800

    Change-Id: I1d1e6f4aa17c06b79d67134546b49b302fc76a08 Signed-off-by: wangymd <wangymd@yonyou.com>

--- src/Demo01/mmm.java
+++ src/Demo01/mmm.java
@@ -5,6 +5,7 @@ public class mmm {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("rebase test!!");
+		System.out.println("rebase test!!");
 	}
 
 }
