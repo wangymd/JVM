189190e87f3ce856b2a9877aae2340e1302d00e2
wangymd <wangymd@yonyou.com>
2016-10-11 17:45:24 +0800

    创建demo02类
    Change-Id: Ic3bf15b8654d972b3a1184a75931de1c8e5229bd
    Signed-off-by: wangymd <wangymd@yonyou.com>

---
+++ src/Demo01/demo02.java
@@ -0,0 +1,12 @@
+package Demo01;
+
+public class demo02 {
+
+	public static void main(String[] args) {
+		String string = "123456";
+		System.out.println(string);
+		System.out.println("µÚÒ»´Î");
+
+	}
+
+}
